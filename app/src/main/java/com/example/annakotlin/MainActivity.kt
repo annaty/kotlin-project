e2444974d1d215ea3732de07d859da09e6054d89
Anna Tylkowska <tylkowskanna@gmail.com>
2023-03-19 16:45:21 +0100

    added header & homepage buttons

--- app/src/main/java/com/example/annakotlin/MainActivity.kt
+++ app/src/main/java/com/example/annakotlin/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.annakotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setHeaderTitle(getString(R.string.app_name))
+
+    }
+
+    fun setHeaderTitle( title:String?){
+        val textViewTitle= findViewById<TextView>(R.id.textViewTitle)
+        textViewTitle.setText(title)
+    }
+
+    fun showBack(){
+        val imageViewBack=findViewById<ImageView>(R.id.imageViewBack)
+        imageViewBack.visibility= View.VISIBLE
+        imageViewBack.setOnClickListener(View.OnClickListener {
+            finish()
+        })
     }
 }
